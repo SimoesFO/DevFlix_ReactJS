dea2b4060d59fecfd9d873dbe05943225800d707
unknown <ti7@orthodonticbrasil.com.br>
2020-07-28 21:35:23 -0300

    Routes, StyledComponents

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import Logo from '../../assets/img/Logo.png'
-import './Menu.css'
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
 //import ButtonLink from './components/ButtonLink';
-import Button from '../Button'
+import Button from '../Button';
 
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <a to="/">
         <img src={Logo} className="Logo" title="Dev Flix" alt="Dev Flix" />
       </a>
       
-      <Button as='a' className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo Ví­deo
       </Button>
 
